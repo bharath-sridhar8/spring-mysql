598567d2d24335ade7ed96771a62a4221b8b6d98
Bharath Sridhar <bharath.sridhar8@gmail.com>
2022-02-24 11:11:57 +0530

    reference to docs

--- accessing-data-mysql/src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplication.java
+++ accessing-data-mysql/src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // sql in the root of the classpath is executed on startup if Hibernate creates the schema from scratch
 // (that is, if the ddl-auto property is set to create or create-drop).
-
+// https://docs.spring.io/spring-boot/docs/current/reference/html/howto.html#howto.data-initialization
 @SpringBootApplication
 public class AccessingDataMysqlApplication {
 
