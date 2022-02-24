be4ab9cc0c4c5a14987235b658f5dd1ba2005fbd
Bharath Sridhar <bharath.sridhar8@gmail.com>
2022-02-24 11:08:46 +0530

    Note on Auto creation of schema on startup

--- accessing-data-mysql/src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplication.java
+++ accessing-data-mysql/src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplication.java
@@ -3,6 +3,9 @@ package com.example.accessingdatamysql;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// sql in the root of the classpath is executed on startup if Hibernate creates the schema from scratch
+// (that is, if the ddl-auto property is set to create or create-drop).
+
 @SpringBootApplication
 public class AccessingDataMysqlApplication {
 
